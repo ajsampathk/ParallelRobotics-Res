3aba05bb2f43ec0f46a74724c4e7b0fba9b48c93
Abhijtih S <aj.sampathk@gmail.com>
2021-05-05 14:51:50 +0530

    Range Bind for acos

--- MatLab/CosineLaw.m
+++ MatLab/CosineLaw.m
@@ -3,5 +3,6 @@ function theta = CosineLaw(adj,hyp,opp)
     numerator = (adj^2)+(hyp^2)-(opp^2);
     denominator = (2*adj*hyp);
     ratio = numerator/denominator;
+    ratio = max(min(ratio,1),-1)
     theta = acos(ratio);
 end
