2befba8227b59c0375c4c69e8a8288db8a25ce10
Rani <ranikinnal44@gmail.com>
2021-05-09 13:40:43 +0530

    files deleted

--- MatLab/CosineLaw.m
+++
@@ -1,8 +0,0 @@
-function theta = CosineLaw(adj,hyp,opp)
-    
-    numerator = (adj^2)+(hyp^2)-(opp^2);
-    denominator = (2*adj*hyp);
-    ratio = numerator/denominator;
-    ratio = max(min(ratio,1),-1)
-    theta = acos(ratio);
-end
